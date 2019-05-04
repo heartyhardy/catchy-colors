8196cba91f1c600ee297b5cb6e8b864dbbf0bd9b
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-05-04 14:27:38 +0530

    Added hue-picker CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ColorSlot from './components/color-slot/color-slot';
+import HuePicker from './components/color-picker/hue-picker/hue-picker';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <ColorSlot hue={200} saturation={45} brightness={50} hexcolor="#2df3d0"></ColorSlot>
+        <HuePicker />
       </div>
     );
   }
--- src/components/color-picker/hue-picker/hue-picker.js
+++ src/components/color-picker/hue-picker/hue-picker.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './hue-picker.css';
 import Aux from '../../hoc/auxilary';
 
-const HuePicker =props => (
+const HuePicker = props => (
     <Aux
-        className=""
+        className="hue-picker-main"
     >
-
+        <p
+            className="hue-range-indictor"
+        >
+            0
+        </p>
+        
+        <input
+            className="hue-range"
+            type="range"
+            min="0"
+            max="360"
+        />
     </Aux>
 )
 
