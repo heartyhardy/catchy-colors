bff5c9f7edb450708c986a733cb0ce836650ca84
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-05-07 00:56:08 +0530

    Fixed bugs in prioriy and ColorPicker, added State and functionality to select colors

--- src/components/color-picker/color-picker.js
+++ src/components/color-picker/color-picker.js
@@ -1,36 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './color-picker.css';
 import Aux from '../hoc/auxilary';
-import ColorSlot from '../color-slot/color-slot';
+import ColorSlots from '../color-slots/color-slots';
 import HuePicker from './hue-picker/hue-picker';
 import SaturationPicker from './saturation-picker/saturation-picker';
 import BrightnessPicker from './brightness-picker/brightness-picker';
 
 const ColorPicker = props => {
 
-    const [hue, setHue] = useState(0);
-    const [saturation, setSaturation] = useState(0);
-    const [brightness, setBrightness] = useState(0);
-    const [priority, setPriority] = useState(1);
+    const [h, setH] = useState(0);
+    const [s, setS] = useState(0);
+    const [l, setL] = useState(0);
+    const [priority, setPriority] = useState({ index: 1 });
+    const [primary, setPrimary] = useState({ h: 0, s: 0, l: 0 });
+    const [secondary, setSecondary] = useState({ h: 0, s: 0, l: 0 });
+    const [tertiary, setTertiary] = useState({ h: 0, s: 0, l: 0 });
+
+    useEffect(() => {
+        getPreviousValues(priority.index);
+    }, [priority])
 
     const onHueChanged = event => {
         const val = event.target.value;
-        setHue((val < 0 || val > 360) ? 0 : val);
+        setH((val < 0 || val > 360) ? 0 : val);
+
+        switch (Number(priority.index)) {
+            case 1:
+                setPrimary(prev => { return { ...prev, h: h } });
+                break;
+            case 2:
+                setSecondary(prev => { return { ...prev, h: h } });
+                break;
+            case 3:
+                setTertiary(prev => { return { ...prev, h: h } });
+                break;
+            default:
+                throw new Error("Invalid value. Unexpected error occured!");
+        }
     }
 
     const onSaturationChanged = event => {
         const val = event.target.value;
-        setSaturation((val < 0 || val > 100) ? 0 : val);
+        setS((val < 0 || val > 100) ? 0 : val);
+
+        switch (Number(priority.index)) {
+            case 1:
+                setPrimary(prev => { return { ...prev, s: s } });
+                break;
+            case 2:
+                setSecondary(prev => { return { ...prev, s: s } });
+                break;
+            case 3:
+                setTertiary(prev => { return { ...prev, s: s } });
+                break;
+            default:
+                throw new Error("Invalid value. Unexpected error occured!");
+        }
     }
 
     const onBrightnessChanged = event => {
         const val = event.target.value;
-        setBrightness((val < 0 || val > 100) ? 0 : val);
+        setL((val < 0 || val > 100) ? 0 : val);
+
+        switch (Number(priority.index)) {
+            case 1:
+                setPrimary(prev => { return { ...prev, l: l } });
+                break;
+            case 2:
+                setSecondary(prev => { return { ...prev, l: l } });
+                break;
+            case 3:
+                setTertiary(prev => { return { ...prev, l: l } });
+                break;
+            default:
+                throw new Error("Invalid value. Unexpected error occured!");
+        }
     }
 
     const onSelectPriority = event => {
         const val = event.target.getAttribute("priority");
-        setPriority(val);
+        const index = { index: val };
+        setPriority(index);
+    }
+
+    const getPreviousValues = () => {
+        switch (Number(priority.index)) {
+            case 1:
+                setH(primary.h);
+                setS(primary.s);
+                setL(primary.l);
+                break;
+            case 2:
+                setH(secondary.h);
+                setS(secondary.s);
+                setL(secondary.l);
+                break;
+            case 3:
+                setH(tertiary.h);
+                setS(tertiary.s);
+                setL(tertiary.l);
+                break;
+            default:
+                throw new Error("Invalid value. Unexpected error occured!");
+        }
     }
 
     return (
@@ -38,29 +110,28 @@ const ColorPicker = props => {
             className="color-picker-main"
         >
 
-            <ColorSlot
-                hue={hue}
-                saturation={saturation}
-                brightness={brightness}
-                priority={1}
-                isActive={priority===1 ? true : false}
+            <ColorSlots
+                primary={primary}
+                secondary={secondary}
+                tertiary={tertiary}
+                priority={Number(priority.index)}
                 onSelectPriority={onSelectPriority}
             />
 
             <HuePicker
-                hue={hue}
+                hue={h}
                 valueChanged={onHueChanged}
             />
             <SaturationPicker
-                hue={hue}
-                saturation={saturation}
-                brightness={brightness}
+                hue={h}
+                saturation={s}
+                brightness={l}
                 valueChanged={onSaturationChanged}
             />
             <BrightnessPicker
-                hue={hue}
-                saturation={saturation}
-                brightness={brightness}
+                hue={h}
+                saturation={s}
+                brightness={l}
                 valueChanged={onBrightnessChanged}
             />
         </Aux>
--- src/components/color-slots/color-slots.js
+++ src/components/color-slots/color-slots.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './color-slots.css';
 import Aux from '../hoc/auxilary';
+import ColorSlot from '../color-slot/color-slot';
 
 const ColorSlots = props => {
     return (
         <Aux
-            className=""
+            className="color-slots-main"
         >
+            <ColorSlot
+                hue={props.primary.h}
+                saturation={props.primary.s}
+                brightness={props.primary.l}
+                priority={1}
+                isActive={props.priority === 1 ? true : false}
+                onSelectPriority={props.onSelectPriority}
+            />
+
+            <ColorSlot
+                hue={props.secondary.h}
+                saturation={props.secondary.s}
+                brightness={props.secondary.l}
+                priority={2}
+                isActive={props.priority === 2 ? true : false}
+                onSelectPriority={props.onSelectPriority}
+            />
+
+            <ColorSlot
+                hue={props.tertiary.h}
+                saturation={props.tertiary.s}
+                brightness={props.tertiary.l}
+                priority={3}
+                isActive={props.priority === 3 ? true : false}
+                onSelectPriority={props.onSelectPriority}
+            />
 
         </Aux>
     )
